8dd5427151781188ea6f8a34ef82b0bc1fd636c2
Saran_Techie03 <79828722+SARAN0308@users.noreply.github.com>
2021-05-06 13:48:52 +0530

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -3,45 +3,57 @@ public class StudentFees {
 String studentname;
 int rollnumber;
 int Class;
-char section;
+String section;
 String schoolname;
-String result;
-public StudentFees(String a,int b,int c,char d, String e,String f) {
-	studentname=a;
-	rollnumber=b;
-	Class=c;
-	section-=d;
-	schoolname=e;
-	result=f;
+boolean result;
+public StudentFees(String studentname,int rollnumber,int Class,String section, String schoolname,boolean result) {
+this.studentname=studentname;
+this.rollnumber=rollnumber;
+this.Class=Class;
+this.section=section;
+this.schoolname=schoolname;
+this.result=result;
 }
-void displayprofileof() {
+ public void displaydata() {
 System.out.println("the student name is:"+studentname);
 System.out.println(" roll  number is:"+rollnumber);
 System.out.println(" class is:"+Class);
 System.out.println("section is:"+section);
 System.out.println("the school name is:"+schoolname);
 System.out.println("the result is:"+result);
-
-
 }
-public boolean check() 
+ public void displayprofileof(String studentName) {
+if(studentName==this.studentname) {
+System.out.println("need student name is:"+studentname);
+System.out.println("need roll  number is:"+rollnumber);
+System.out.println(" new class is:"+Class);
+System.out.println(" newsection is:"+section);
+System.out.println("the new school name is:"+schoolname);
+}else {
+System.out.println("user not found");
+}
+ }
+public boolean ispass()
 {
-	if(result =="pass") {
-		return true;
-	}
-	else
-	{
-		return false;}
+if(this.result) {
+return true;
 }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		StudentFees s1= new StudentFees("RAJ",1,12,'S',"SELVAM","PASS");
-		StudentFees s2= new StudentFees("kumar",3,10,'d',"mechnet","fail");
-		s1.displayprofileof();
-		s1.check();
-		s2.displayprofileof();
-		s2.check();
-	}
+else
+{
+return false;}
+}
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+StudentFees s1= new StudentFees("raj",1,12,"c","selvam",true);
+s1.displaydata();
+s1.displayprofileof("ramesh");
+System.out.println("is student pass:"+s1.ispass());
+StudentFees s2= new StudentFees("kumar",3,10,"d","mechnet",false);
+s2.displaydata();
+s2.displayprofileof("kumar");
+System.out.println("is student pass:"+s2.ispass());
+
 
 }
 
+}
